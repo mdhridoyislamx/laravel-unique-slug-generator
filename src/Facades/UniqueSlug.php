f615ca74ac6eb043d02edcc34352448edfca8cc3
Md Hridoy Islam <149479335+mdhridoyislamx@users.noreply.github.com>
2024-05-08 13:48:12 +0600

    Add files via upload

--- src/Facades/UniqueSlug.php
+++ src/Facades/UniqueSlug.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace Hridoy\LaravelUniqueSlug\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Hridoy\LaravelUniqueSlug\UniqueSlug
+ */
+class UniqueSlug extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'laravel-unique-slug';
+    }
+}
